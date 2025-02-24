7ae80608837184de5e3483f398e602678f50dda0
vennreddy490 <vennreddy490@gmail.com>
2025-02-23 22:37:46 -0500

    Base carousel added, reworking now to add better styling

--- src/pages/MissionOverviewPage.jsx
+++ src/pages/MissionOverviewPage.jsx
@@ -1,4 +1,5 @@
 import Topbar from '../components/Topbar';
+import Carousel from '../components/Carousel'
 
 function MissionOverViewPage() {
     return (
@@ -6,6 +7,13 @@ function MissionOverViewPage() {
             <Topbar></Topbar>
             <hr className="border-t border-white w-full" />
             <h1>Mission Overview</h1>
+            <div className="mt-4 max-w-4xl mx-auto h-64">
+                <Carousel>
+                <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/1.jpg" width="300" height="300" alt="Slide 1" className="w-full" />
+                <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/2.jpg" width="300" height="300" alt="Slide 2" className="w-full" />
+                <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/3.jpg" width="300" height="300" alt="Slide 3" className="w-full" />
+                </Carousel>
+            </div>
         </div>
     );
 }
