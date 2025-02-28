a2b6d403c6d65911094b5ac69a3690816b617044
vennreddy490 <vennreddy490@gmail.com>
2025-02-28 14:33:57 -0500

    :construction: Mission Overview page revamped, bg added, first card (template of many) added

--- src/pages/MissionOverviewPage.jsx
+++ src/pages/MissionOverviewPage.jsx
@@ -1,11 +1,21 @@
 import Topbar from '../components/Topbar';
 import Carousel from '../components/Carousel'
+import Background from '../assets/images/space_bg_3.jpg';
+import Fact_Card_1 from '../assets/images/fact_cards/mo_fc_1.png';
 
 function MissionOverViewPage() {
     return (
         // Outer container, this takes the full viewport height and hides the overflow
         // <div className="h-screen overflow-hidden flex flex-col">
-        <div className="h-screen flex flex-col">
+        <div
+            className="h-screen flex flex-col bg-cover bg-center"
+            style={{
+                backgroundImage: `url(${Background})`,
+                backgroundSize: "cover",       // Makes the image cover the entire area
+                backgroundRepeat: "no-repeat", // Prevents tiling
+                backgroundPosition: "center"   // Centers the image
+            }}
+        >
             <Topbar></Topbar>
             <hr className="border-t border-white w-full" />
             <h1>Mission Overview</h1>
@@ -15,7 +25,8 @@ function MissionOverViewPage() {
                 {/* <div className="w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 aspect-square"> */}
                 <div className="w-11/12 sm:w-3/4 md:w-2/3 lg:w-4/10 aspect-square">
                     <Carousel>
-                    <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/1.jpg" alt="Slide 1" className="w-full h-full object-cover" />
+                    {/* <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/1.jpg" alt="Slide 1" className="w-full h-full object-cover" /> */}
+                    <img src={Fact_Card_1} alt="Slide 1" className="w-full h-full object-cover" />
                     <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/2.jpg" alt="Slide 2" className="w-full h-full object-cover" />
                     <img src="https://juazeiro.gteducacao.com.br/assets/images/samples/300x300/3.jpg" alt="Slide 3" className="w-full h-full object-cover" />
                     </Carousel>
